a0d9ff211e1b21c08aff6214122c4bf163974ffe
Maiyoj <vmaiyo@btn-solutions.co.ke>
2022-03-29 22:15:39 +0300

    Routeprotection

--- routes/api.php
+++ routes/api.php
@@ -20,24 +20,26 @@ use Illuminate\Support\Facades\Route;
     return  products::all();
 });*/
 
-
-Route::resource('products', ProductsController::class);
-
-
+//public Routes
+//Route::resource('products', ProductsController::class);
 Route::get('/products/search/{name}', [ProductsController::class, 'search']);
+Route::get('/products/search/{id}', [ProductsController::class, 'show']);
+Route::get('/products', [ProductsController::class, 'index']);
+
+
+//Protected Routes
+Route::group(['middleware'=> ['auth:sanctum']],function(){
+Route::post('/products', [ProductsController::class, 'store']);
+Route::put('/products{$id}', [ProductsController::class, 'update']);
+Route::delete('/products{$id}', [ProductsController::class, 'destroy']);
+});
+
 
 /*
 Route::post('/products', [ProductsController::class, 'store']);
 */
-/*Route::post('/products', function (){
-    return  products::create(
-        [
-            "name" => "Product Tea",
-            "slug" => "Product-Tea",
-            "description" => "Green Tea",
-            "price" => "20"
-        ]);
-});*/
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
